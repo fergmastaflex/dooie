1c3476c89ba29dc86488e7e265f61402bc1f2d33
fergmastaflex <joey@fergmedia.com>
2015-06-11 01:19:42 -0600

    Add block to delete todo_file after tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,7 @@ Bundler.require(:default, :development, :test)
 
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
+  config.after(:suite) do
+    FileUtils.rm_rf(Dir["**/todo_items.txt"])
+  end
 end
